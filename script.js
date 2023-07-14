5c0c8597fed8b67d22257b60323d0d72dcfef8ad
Maya <106397404+MayaChena@users.noreply.github.com>
2023-07-14 02:07:39 -0400

    add grid change with user interaction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const grid = document.querySelector('#grid');
 
 function createGrid(rows, columns){
-    grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-    grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-
+    grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    
     for (let i = 0; i < rows * columns; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
@@ -11,6 +11,24 @@ function createGrid(rows, columns){
     };
 };
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    createGrid(16, 16);
+let isMouseDown = false;
+
+grid.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+
+grid.addEventListener('mouseover', (e) => {
+    if (!isMouseDown) return;
+
+    if (isMouseDown) {
+        e.target.style.backgroundColor = 'darkgrey';
+    };
+});
+
+grid.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid(24, 24);
 });
